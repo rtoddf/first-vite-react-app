4a145df471357db7975a637ac656538b74f46dfd
rtoddf <rtoddf@me.com>
2022-05-16 20:41:17 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Card from './Components/Card/default';
+import './styles/base.scss'
+
+function App() {
+  return (
+    <div className="container">
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Card/default.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import person from './data/cards.json';
+import './default.scss';
+
+function Card() {
+    return (
+        <div className="card">
+            <figure>
+                <img className="round" src={person.image} alt={person.name} />
+            </figure>
+
+            <h4>{person.name}</h4>
+            <h5>{person.show}</h5>
+
+            {person.social.map((iconInfo, id) => {
+                return (
+                    <a key={id} href={iconInfo.link} target="_blank">
+                        <i className={`${iconInfo.icon} rotate`}></i>
+                    </a>
+                )
+            })}
+        </div>
+    );
+}
+
+export default Card;
