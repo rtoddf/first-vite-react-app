f92db5b532b0798347efa5ebb0f5c64dcefc2eb3
rtoddf <rtoddf@me.com>
2023-07-04 12:35:28 -0400

    installed and did a button

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,30 @@ import { Container } from './assets/styles/Container.styled';
 import GridSimple from './Layouts/GridSimple';
 import Card from './Components/Card/default';
 
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const consoleMessage = (message) => {
+  console.log(message)
+}
+
 function App() {
   return (
     <ThemeProvider theme={ theme}>
       <GlobalStyles />
       <Container>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="large"
+          // startIcon={<SendIcon />}
+          endIcon={<DeleteIcon />}
+          // style={{fontSize: 32}}
+          onClick={() => consoleMessage('this is the message')}
+        >
+          Good Afternoon
+        </Button>
         <GridSimple cols={`6`}>
           <Card image="./images/casey-cott.jpg" name="Casey Cott" show="Riverdale" />
           <Card image="./images/casey-cott.jpg" name="Casey Cott" show="Riverdale" />
