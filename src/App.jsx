86a8b4ffedad5f052794671283abd0b910bf9c79
rtoddf <rtoddf@me.com>
2024-10-16 12:48:38 -0400

    fixed some issues-todd

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,97 @@
 import React, { useEffect } from 'react';
 import * as THREE from 'three';
-// import { GUI } from 'dat.gui';
+import { GUI } from 'dat.gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import './styles.css';
 
 function App() {
   useEffect(() => {
-    // const gui = new dat.GUI();
+    const gui = new GUI();
+    // gui.add(directionalLight, 'intensity', 0, 10);
+    // gui.add(directionalLight.position, 'x', 0, 20);
+    // gui.add(directionalLight.position, 'y', 0, 20);
+    // gui.add(directionalLight.position, 'z', 0, 20);
+    // const colorFolder = gui.addFolder('Color');
+    // const color = colorFolder.addColor(geometryBaseColor, 'hex');
+
+    const container = document.getElementById('shape-holder');
+    const canvasWidth = container.offsetWidth;
+    const canvasHeight = container.offsetHeight;
+
+    console.log('canvasWidth: ', canvasWidth);
 
     // create a renderer
-    const canvas = document.getElementById('myThreeJSCanvas');
     const renderer = new THREE.WebGLRenderer({
-      canvas,
+      container,
       antialias: true,
       alpha: true,
     });
 
     renderer.setClearColor(0xffffff);
     renderer.setPixelRatio(devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+    renderer.setSize(canvasWidth, canvasHeight);
+    // container.removeChild(renderer.domElement);
+    container.appendChild(renderer.domElement);
 
     // create a scene
     const scene = new THREE.Scene();
+    scene.clear();
 
     // create a camera
     const camera = new THREE.PerspectiveCamera(
       1000, // field of view
-      window.innerWidth / window.innerHeight, // aspect ratio
+      canvasWidth / canvasHeight, // aspect ratio
       1, // near
       5000 // far
     );
 
-    const orbit = new OrbitControls(camera, renderer.domElement);
-    // orbit.enableZoom = false;
-
     // position the camera so you're not on top of the geometry
     camera.position.x = 0;
     camera.position.y = 0;
-    camera.position.z = 400;
+    camera.position.z = 200;
     scene.add(camera);
 
-    // create lights
-    const pointtLight01 = new THREE.AmbientLight(0xff7700, 2, 2000);
-    // ambientLight.castShadow = true;
-    pointtLight01.position.set(200, 0, 200);
-    scene.add(pointtLight01);
-
-    const pointtLight02 = new THREE.AmbientLight(0xff7700, 2, 2000);
-    // spotLight.castShadow = true;
-    pointtLight02.position.set(0, 0, 200);
-    scene.add(pointtLight02);
+    const orbit = new OrbitControls(camera, renderer.domElement);
+    // orbit.enableZoom = false;
 
     // create geometry
-    const boxGeometry = new THREE.BoxGeometry(100, 100, 100);
-    const boxMaterial = new THREE.MeshPhongMaterial({
-      color: 0xff0000,
+    const geometry = new THREE.BoxGeometry(100, 100, 100);
+    // create a shiny material
+    const material = new THREE.MeshPhongMaterial({
+      color: 0xffffff,
     });
-    // const boxMaterial = new THREE.MeshNormalMaterial();
-    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    scene.add(boxMesh);
+    //create a mesh, which takes a geometry and a material
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
-    const animate = () => {
-      boxMesh.rotation.x += 0.03;
-      boxMesh.rotation.z += 0.03;
+    // create lights
+    const light01 = new THREE.PointLight(0xff7700, 1, 200);
+    // light01.castShadow = true;
+    light01.position.set(300, 300, 300);
+
+    const light02 = new THREE.PointLight(0xae0000, 1, 200);
+    // light02.castShadow = true;
+    light02.position.set(300, 300, 300);
+
+    scene.add(light01);
+    scene.add(light02);
+
+    const render = () => {
+      animate();
+      requestAnimationFrame(render);
+      renderer.clear();
       renderer.render(scene, camera);
-      window.requestAnimationFrame(animate);
     };
 
-    animate();
+    const animate = () => {
+      mesh.rotation.x += 0.03;
+      mesh.rotation.z += 0.03;
+    };
+
+    render();
   }, []);
 
-  return (
-    <div id="holder">
-      <canvas id="myThreeJSCanvas" />
-    </div>
-  );
+  return <div id="shape-holder"></div>;
 }
 
 export default App;
