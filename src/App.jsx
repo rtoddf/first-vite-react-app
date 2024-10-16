62b3358c21f008ede440bedfd7692e18c99af0eb
rtoddf <rtoddf@me.com>
2024-10-16 10:41:32 -0400

    cleaning some stuff up-todd

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import React, { useEffect } from 'react';
 import * as THREE from 'three';
+// import { GUI } from 'dat.gui';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 function App() {
   useEffect(() => {
-    const scene = new THREE.Scene();
+    // const gui = new dat.GUI();
 
-    const camera = new THREE.PerspectiveCamera(
-      1000,
-      window.innerWidth / window.innerHeight,
-      1,
-      1000
-    );
-    camera.position.x = 0;
-    camera.position.y = 0;
-    camera.position.z = 100;
-
+    // create a renderer
     const canvas = document.getElementById('myThreeJSCanvas');
     const renderer = new THREE.WebGLRenderer({
       canvas,
@@ -22,22 +15,48 @@ function App() {
       alpha: true,
     });
 
-    // renderer.setClearColor(0xffffff);
-    // renderer.setPixelRatio(devicePixelRatio);
+    renderer.setClearColor(0xffffff);
+    renderer.setPixelRatio(devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    ambientLight.castShadow = true;
-    scene.add(ambientLight);
+    // create a scene
+    const scene = new THREE.Scene();
 
-    const spotLight = new THREE.SpotLight(0xffffff, 1);
-    spotLight.castShadow = true;
-    spotLight.position.set(0, 64, 32);
-    scene.add(spotLight);
+    // create a camera
+    const camera = new THREE.PerspectiveCamera(
+      1000, // field of view
+      window.innerWidth / window.innerHeight, // aspect ratio
+      1, // near
+      5000 // far
+    );
 
-    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
-    const boxMaterial = new THREE.MeshNormalMaterial();
+    const orbit = new OrbitControls(camera, renderer.domElement);
+    // orbit.enableZoom = false;
+
+    // position the camera so you're not on top of the geometry
+    camera.position.x = 0;
+    camera.position.y = 0;
+    camera.position.z = 400;
+    scene.add(camera);
+
+    // create lights
+    const pointtLight01 = new THREE.AmbientLight(0xff7700, 2, 2000);
+    // ambientLight.castShadow = true;
+    pointtLight01.position.set(200, 0, 200);
+    scene.add(pointtLight01);
+
+    const pointtLight02 = new THREE.AmbientLight(0xff7700, 2, 2000);
+    // spotLight.castShadow = true;
+    pointtLight02.position.set(0, 0, 200);
+    scene.add(pointtLight02);
+
+    // create geometry
+    const boxGeometry = new THREE.BoxGeometry(100, 100, 100);
+    const boxMaterial = new THREE.MeshPhongMaterial({
+      color: 0xff0000,
+    });
+    // const boxMaterial = new THREE.MeshNormalMaterial();
     const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
     scene.add(boxMesh);
 
